30b7f7ddf1184658bd742b7c7f9501e7056c61ce
ngoc.nguyenquang <ngoc.nguyenquang@asnet.com.vn>
2022-12-23 11:28:20 +0700

    Reset state with key

--- my-react-app/src/Hooks/useState/index.tsx
+++ my-react-app/src/Hooks/useState/index.tsx
@@ -17,41 +17,71 @@ const FormStateHook = () => {
 
 export default FormStateHook;
 
+// Passing initializer function
 const createInitialTodos = () => {
   const initialTodos = [];
   for (let i = 0; i < 50; i++) {
     initialTodos.push({
       id: i,
-      text: 'Item ' + (i + 1)
+      text: "Item " + (i + 1),
     });
   }
   return initialTodos;
-}
+};
 
 const TodoList = () => {
   const [todos, setTodos] = useState(createInitialTodos);
-  const [text, setText] = useState('');
+  const [text, setText] = useState("");
 
   return (
     <>
-      <input
-        value={text}
-        onChange={e => setText(e.target.value)}
-      />
-      <button onClick={() => {
-        setText('');
-        setTodos([{
-          id: todos.length,
-          text: text
-        }, ...todos]);
-      }}>Add</button>
+      <input value={text} onChange={(e) => setText(e.target.value)} />
+      <button
+        onClick={() => {
+          setText("");
+          setTodos([
+            {
+              id: todos.length,
+              text: text,
+            },
+            ...todos,
+          ]);
+        }}
+      >
+        Add
+      </button>
       <ul>
-        {todos.map(item => (
-          <li key={item.id}>
-            {item.text}
-          </li>
+        {todos.map((item) => (
+          <li key={item.id}>{item.text}</li>
         ))}
       </ul>
     </>
   );
-}
+};
+
+// Reset state with key
+const Form = () => {
+  const [name, setName] = useState("Taylor");
+
+  return (
+    <>
+      <input value={name} onChange={(e) => setName(e.target.value)} />
+      <p>Hello, {name}.</p>
+    </>
+  );
+};
+
+const FormContainer = () => {
+  const [version, setVersion] = useState(0);
+
+  const handleReset = () => {
+    setVersion(version + 1);
+  };
+
+  return (
+    <>
+      <button onClick={handleReset}>Reset</button>
+      <Form key={version} />
+    </>
+  );
+};
