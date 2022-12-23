dad3495c7eb455ae45d6d6814f14c98efea9bc21
ngoc.nguyenquang <ngoc.nguyenquang@asnet.com.vn>
2022-12-23 10:03:53 +0700

    useState example

---
+++ my-react-app/src/Hooks/useState/index.tsx
@@ -0,0 +1,18 @@
+import { useState } from "react";
+
+const FormStateHook = () => {
+  const [name, setName] = useState("Taylor");
+  const [age, setAge] = useState(42);
+
+  return (
+    <>
+      <input value={name} onChange={(e) => setName(e.target.value)} />
+      <button onClick={() => setAge(age + 1)}>Increment age</button>
+      <p>
+        Hello, {name}. You are {age}.
+      </p>
+    </>
+  );
+};
+
+export default FormStateHook;
