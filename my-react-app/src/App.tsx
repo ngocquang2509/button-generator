f048efd477c8ac3d600fa4127b488e9ed0f3ec1f
ngoc.nguyenquang <ngoc.nguyenquang@asnet.com.vn>
2022-12-27 17:17:27 +0700

    useMemo example

--- my-react-app/src/App.tsx
+++ my-react-app/src/App.tsx
@@ -1,4 +1,4 @@
-import { Profiler } from "react";
+import { Profiler, useMemo, useState } from "react";
 import HoverImage1 from "./Advance/HOC/Image1";
 import HoverImage2 from "./Advance/HOC/Image2";
 import NewForm from "./Advance/Profiler/NewForm";
@@ -8,13 +8,24 @@ import { Effect } from "./Hooks/useEffect";
 import CounterReducer from "./Hooks/useReducer";
 import { Greeting } from "./Main Concepts/Conditional Rendering";
 
-function App() {
+const App = () => {
+  const [count, setCount] = useState(0);
+
+  const handleClick = () => {
+    setCount(count + 1);
+  };
+
+  const memoizedComponent = useMemo(() => {
+    return <Greeting isLoggedIn={true} />;
+  }, []);
+
   return (
     <div className="App">
-      <Greeting isLoggedIn={true} />
-      <CounterReducer />
+      <h2>You clicked {count} times</h2>
+      <button onClick={handleClick}>Click me !</button>
+      {memoizedComponent}
     </div>
   );
-}
+};
 
 export default App;
